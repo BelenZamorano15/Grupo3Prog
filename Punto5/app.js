297bd76d1ba6d4e2873a9e24c026d07e2dc347ec
Belén Zamorano <mariabelenzamoranoo@gmail.com>
2025-04-29 16:21:48 +0200

    Delete Punto5 directory

--- Punto5/app.js
+++
@@ -1,5 +0,0 @@
-//Realizado por: Belén Zamorano.
-
-let numero1 = (Number(prompt("Ingrese un número")));
-let numero2 = (Number(prompt("Ingrese otro número")));
-alert("La suma de los números es: " + (numero1+numero2));
