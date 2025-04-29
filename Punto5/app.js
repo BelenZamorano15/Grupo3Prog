37c1bc11bfbd269f5f0658f1db47325e4d37ed28
Belén Zamorano <mariabelenzamoranoo@gmail.com>
2025-04-29 11:11:25 -0300

    Add files via upload

---
+++ Punto5/app.js
@@ -0,0 +1,5 @@
+//Realizado por: Belén Zamorano.
+
+let numero1 = (Number(prompt("Ingrese un número")));
+let numero2 = (Number(prompt("Ingrese otro número")));
+alert("La suma de los números es: " + (numero1+numero2));
