cbc5dd5ce140472a947d7e21f3d5b24b717eaf05
Lunaluz25 <luzmavic17@gmail.com>
2025-04-29 21:59:04 -0300

    Agrego ejercicio 8

---
+++ Punto8/app.js
@@ -0,0 +1,8 @@
+// Realizado por: Luz Luna
+
+let num = parseInt(prompt("Ingrese un numero: "));
+if (num % 2 == 0) {
+    console.log ("El numero es divisible en 2");
+} else {
+    console.log ("El numero no es divisible en 2");
+}
